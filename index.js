ee1df7605ef580c8ccc1b1e00bdfc21b394c80a6
Anil pandey <anilpandey071999@gmail.com>
2020-04-03 01:07:14 +0530

    add stock api iexcloud

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,11 +10,40 @@ const PORT = process.env.PORT || 5000;
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// API key pk_a5f10f84b6e54750a8849f64f87c4aa5
+// function call_api(finishedAPI){
+// 	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_a5f10f84b6e54750a8849f64f87c4aa5', {json: true}, (err , res, body) => {
+// 		if(err) {return console.log(err);}
+// 		//console.log(body);
+// 		if(res.statusCode === 2000){
+// 			finishedAPI(body);
+// 		};
+// 	});
+// };
+
+function call_api(finishedAPI) {
+	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_062031d20883444f9ea74e2610fe2011', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
 //set handlebar routes
 app.get('/', function (req, res) {
-    res.render('home',{
-    	stuff:"This is stuff"
+    call_api(function(doneAPI) {
+    	res.render('home',{
+    		stuff: doneAPI
+    	});
     });
+    
+});
+
+//set about apge
+app.get('/about.html',function(req,res){
+	res.render('about');
 });
 
 //set static folder
